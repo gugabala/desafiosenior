eb0ce61936369fa23c9b6ac02337b8bdc3ba5296
Your Name <gugabala@gmail.com>
2024-08-07 17:36:30 -0300

    swagger

--- desafiobackend/src/main/java/br/com/senior/desafiobackend/controller/TarefaController.java
+++ desafiobackend/src/main/java/br/com/senior/desafiobackend/controller/TarefaController.java
@@ -3,6 +3,7 @@ package br.com.senior.desafiobackend.controller;
 import br.com.senior.desafiobackend.db.dto.TarefaDTO;
 import br.com.senior.desafiobackend.exception.ResourceNotFoundException;
 import br.com.senior.desafiobackend.service.TarefaService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,11 +20,13 @@ public class TarefaController {
     private TarefaService tarefaService;
 
     @GetMapping
+    @Operation(summary  = "Retorna todas as tarefas ordenadas por status e data de criação")
     public List<TarefaDTO> getAllTarefas() {
         return tarefaService.getAllTarefas();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary   = "Retorna uma tarefa pelo id")
     public ResponseEntity<TarefaDTO> getTarefaById(@PathVariable Long id) {
         try {
             TarefaDTO tarefa = tarefaService.getTarefaById(id);
@@ -34,12 +37,14 @@ public class TarefaController {
     }
 
     @PostMapping
+    @Operation(summary = "Cria uma nova tarefa")
     public ResponseEntity<TarefaDTO> createTarefa(@Valid @RequestBody TarefaDTO tarefaDTO) {
         TarefaDTO createdTarefa = tarefaService.createTarefa(tarefaDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTarefa);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary  = "Atualiza uma tarefa pelo id")
     public ResponseEntity<TarefaDTO> updateTarefa(@PathVariable Long id, @Valid @RequestBody TarefaDTO tarefaDTO) {
         try {
             TarefaDTO updatedTarefa = tarefaService.updateTarefa(id, tarefaDTO);
@@ -50,6 +55,7 @@ public class TarefaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary  = "Deleta uma tarefa pelo id")
     public ResponseEntity<Void> deleteTarefa(@PathVariable Long id) {
         try {
             tarefaService.deleteTarefa(id);
@@ -60,6 +66,7 @@ public class TarefaController {
     }
 
     @PutMapping("/{id}/concluir")
+    @Operation(summary  = "Conclui uma tarefa pelo id")
     public ResponseEntity<TarefaDTO> concluirTarefa(@PathVariable Long id) {
         try {
             TarefaDTO tarefa = tarefaService.concluirTarefa(id);
